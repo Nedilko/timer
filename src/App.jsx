af380f6835f2ed1e696de2433388c696bf105121
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-06 18:22:03 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="flex flex-col h-screen">
+      <header className="bg-red-500 flex justify-between">
+        <div>settings</div>
+        <div>theme</div>
+      </header>
+      <main className="bg-gray-200">
+        <div>App header</div>
+        <div>
+          <div>Timer header</div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
