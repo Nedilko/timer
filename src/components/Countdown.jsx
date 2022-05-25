b0028a3a695656008cbcf6308d26cfc5dcb039a7
Andrii Haranin <tux255@gmail.com>
2022-05-25 20:15:24 +0300

    Fix colon

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -5,19 +5,19 @@ function Countdown({ Date }) {
 
   return (
     <div className="grid grid-cols-4 flex-row flex-wrap items-end justify-around px-10 text-center uppercase">
-      <div className="after:absolute after:text-2xl after:content-[':']">
+      <div className="after:absolute after:text-2xl after:content-[':'] after:absolute after:right-0 after:bottom-0 relative">
         <div>days</div>
         <div className="text-2xl">{date.days()}</div>
       </div>
-      <div className="after:absolute after:text-2xl after:content-[':']">
+      <div className="after:absolute after:text-2xl after:content-[':'] after:absolute after:right-0 after:bottom-0 relative">
         <div>hours</div>
         <div className="text-2xl">{date.hours()}</div>
       </div>
-      <div className="after:absolute after:text-2xl after:content-[':']">
+      <div className="after:absolute after:text-2xl after:content-[':'] after:absolute after:right-0 after:bottom-0 relative">
         <div>minutes</div>
         <div className="text-2xl">{date.minutes()}</div>
       </div>
-      <div className="after:absolute after:text-2xl after:content-[':']">
+      <div>
         <div>seconds</div>
         <div className="text-2xl">{date.seconds()}</div>
       </div>
