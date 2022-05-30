10dfeaf35d220fbc1b72c9a6be4676f775eb6dc5
Andrii Haranin <tux255@gmail.com>
2022-05-30 21:47:55 +0300

    [Feature]: Settings modal dialog HTML markup

---
+++ src/components/Modal.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Modal() {
+  return (
+    <div className='absolute backdrop-blur-sm w-full h-full flex items-center justify-center rounded-xl'>
+      <div className='bg-gray-300 rounded-xl shadow-xl p-2'>
+        <header className='border-b border-gray-500 text-xl py-2'>Settings</header>
+        <main className='flex flex-col py-2'>
+          <span className='py-2'>Greetings text
+            <input className='mx-1 px-1 rounded bg-gray-200' type="text" placeholder="put some text here"/>
+          </span>
+          <span>
+            <span className='py-2'>Day
+              <select className="rounded mx-1" name="" id="">
+                <option value="1">Monday</option>
+                <option value="2">Tuesday</option>
+                <option value="3">Wednesday</option>
+              </select>
+            </span>
+
+            <span className='py-2'>Hour
+              <select className="rounded mx-1" name="" id="">
+                <option value="0">00:00</option>
+                <option value="1">01:00</option>
+                <option value="23">23:00</option>
+              </select>
+            </span>
+          </span>
+          <span className='py-2'>Use system theme</span>
+        </main>
+        <footer className='flex border-t border-gray-500 justify-end  py-2'>
+          <span className='px-2'>Apply</span>
+          <span className='px-2'>Cancel</span>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
