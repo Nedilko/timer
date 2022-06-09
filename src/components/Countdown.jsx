0e5da5df075e9ea13b54234e75207fb7e6c69f32
Andrii Haranin <tux255@gmail.com>
2022-06-09 19:49:00 +0300

    [Feature]: Settings modal dialog HTML markup (#25)
    * [Feature]: Settings modal dialog HTML markup
    
    * WiP: Panel component added
    
    * WiP: Panel component added
    
    * WiP: Remove junk React import
    
    * Return needed imports
    
    * Lost one select
    
    * Not yet needed
    
    * Remove redunant React import
    
    * Remove redunant

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect }  from 'react';
 import { calcDateDuration } from '../utils/dateUtils'
 import PropTypes from 'prop-types'
+import {useEffect, useState} from 'react'
 
 Countdown.propTypes = {
   targetDate: PropTypes.object.isRequired,
--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import PropTypes from 'prop-types'
 
 const bgLight = `bg-[url('./assets/light.svg')]`
