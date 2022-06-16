f44c91dc70270685e50dd38d0503f93f0f83395d
Andrii Haranin <tux255@gmail.com>
2022-06-16 21:23:41 +0300

    fix toggle animation

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 
 const bgLight = `bg-[url('./assets/light.svg')]`
@@ -9,14 +10,19 @@ ToggleSwitch.propTypes = {
 }
 
 function ToggleSwitch({onChange, isOn}) {
+  const [toggleOn, setToggleOn] = useState(isOn)
+  const handleClick = () => {
+    setToggleOn((oldState) => !oldState)
+    onChange()
+  }
   return (
     <span
-      onClick={onChange}
-      className={`dark:bg-gray-6ex00 flex h-5 w-9 cursor-pointer appearance-none rounded-full bg-gray-100 ease-linear ${isOn ? 'bg-left' : 'bg-right'} ${isOn ? bgLight : bgDark} bg-contain bg-[length:20px] bg-no-repeat shadow-sm`}
+      onClick={handleClick}
+      className={`dark:bg-gray-6ex00 flex h-5 w-9 cursor-pointer appearance-none rounded-full bg-gray-100 ease-linear ${toggleOn ? 'bg-left' : 'bg-right'} ${toggleOn ? bgLight : bgDark} bg-contain bg-[length:20px] bg-no-repeat shadow-sm`}
     >
       <span
         className={`${
-          isOn ? 'translate-x-full' : ''
+          toggleOn ? 'translate-x-full' : ''
         } m-0.5 h-4 w-4 rounded-full bg-gray-200
       transition duration-75 ease-linear dark:bg-gray-700`}
       ></span>
