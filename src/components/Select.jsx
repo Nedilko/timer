c11fdc5c1df13aa841d411ed83a1c1e8eba5b71e
Andrii Haranin <tux255@gmail.com>
2022-06-17 13:51:49 +0300

    Select updated to pass data up

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,21 +1,30 @@
-import {useState} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import PropTypes from 'prop-types'
 
 Select.propTypes = {
   label: PropTypes.string,
   options: PropTypes.array.isRequired,
-  selected: PropTypes.number
+  selected: PropTypes.number,
+  onChange: PropTypes.func,
+  name: PropTypes.string.isRequired
 }
 
-function Select({label, selected = null,options}) {
+function Select({name, label, selected = null, onChange, options}) {
+  const refSelect = useRef(null)
   const [value, setValue] = useState(selected)
-  const handleChange = (event) => {
-    setValue(event.target.value)
+  const handleChange = (e) => {
+    setValue(parseInt(refSelect.current.value))
   }
 
+  useEffect(()=>{
+    const selectedOption = {}
+    selectedOption[refSelect.current.name] = value
+    onChange(selectedOption)
+  },[value])
+
   return (
-    <label className='py-2'>{label}
-      <select onChange={handleChange} className="rounded mx-1" name={label} value={value}>
+    <label className='py-2 select-none'>{label}
+      <select ref={refSelect} onChange={handleChange} className="rounded mx-1" name={name} value={value}>
         {options.map((value, i) => <option key={i} value={i}>{value}</option>)}
       </select>
     </label>
