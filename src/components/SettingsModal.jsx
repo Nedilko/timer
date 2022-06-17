833f9bf44c1b6679e4d947bbb6ee42231a64a43b
Andrii Haranin <tux255@gmail.com>
2022-06-17 13:52:46 +0300

    SettingsModal can store settings from child inputs, select, checkbox.

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -4,6 +4,7 @@ import Modal from './Modal'
 import Button from './Button'
 import Select from './Select'
 import Input from './Input'
+import Checkbox from './Checkbox'
 import {settingsData} from '../settings'
 import {loadSettings} from '../utils/storageAdapter'
 
@@ -14,24 +15,36 @@ SettingsModal.propTypes = {
 
 function SettingsModal({onCancel, onApply}) {
   const [settings, setSettings] = useState(loadSettings)
-  const ref = useRef()
 
-  const handleClick = () => {
-    if (ref.current && !ref.current.contains(e.target)) {
-      console.log('click outside')
-    }
+  const handleDateChange = (selectValue) => {
+    setSettings({
+      ...settings,
+      targetDate: {
+        ...settings.targetDate,
+        ...selectValue
+      }
+    })
+  }
+
+  function handleSettingsChange(value) {
+    setSettings({
+      ...settings,
+      ...value
+    })
   }
 
   return (
-    <Modal onClick={handleClick}>
+    <Modal>
       <div className='border-b border-gray-500 text-xl py-2'>Settings</div>
       <div className='flex flex-col py-2'>
-        <Input label="Greetings text"/>
+        <Input name="greetingsText" onChange={handleSettingsChange} label="Greetings text"/>
         <div className='flex flex-col'>
-          <Select selected={settings.targetDate.days} options={settingsData.days} label={'Day'}/>
-          <Select selected={settings.targetDate.hours} options={settingsData.hours} label={'Hour'}/>
+          <Select name='days' onChange={handleDateChange} selected={settings.targetDate.days}
+                  options={settingsData.days} label={'Day'}/>
+          <Select name='hours' onChange={handleDateChange} selected={settings.targetDate.hours}
+                  options={settingsData.hours} label={'Hour'}/>
         </div>
-        <label className='py-2'>Use system theme</label>
+        <Checkbox onChange={handleSettingsChange} name='useSystemTheme'/>
       </div>
       <footer className='flex border-t border-gray-500 justify-end  py-2'>
         <Button title={'Apply'} onClick={onApply}/>
