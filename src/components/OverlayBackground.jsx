63b7d2f4a24d92baa3fe50ea9aa98d748e2e68fe
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-24 21:52:24 +0300

    handle close out off modal

--- src/components/OverlayBackground.jsx
+++ src/components/OverlayBackground.jsx
@@ -1,16 +1,19 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 OverlayBackground.propTypes = {
   children: PropTypes.object,
-  onClick: PropTypes.func
-}
+  onClick: PropTypes.func,
+};
 
-function OverlayBackground({children}) {
+function OverlayBackground({ children, onClick }) {
   return (
-    <div className='fixed inset-0	 backdrop-blur-sm w-full h-full flex items-center justify-center'>
+    <div
+      onClick={onClick}
+      className="fixed inset-0	 backdrop-blur-sm w-full h-full flex items-center justify-center"
+    >
       {children}
     </div>
-  )
+  );
 }
 
-export default OverlayBackground
+export default OverlayBackground;
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -6,10 +6,13 @@ Panel.propTypes = {
 
 function Panel({children}) {
   return (
-    <div className='bg-gray-300 rounded-xl shadow-xl p-2'>
+    <div
+      onClick={(e) => e.stopPropagation()}
+      className="bg-gray-300 rounded-xl shadow-xl p-2"
+    >
       {children}
     </div>
-  )
+  );
 }
 
 export default Panel
