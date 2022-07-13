8b41d03031c95be21c3560641ac5b4bc3f7812ad
Andrii Haranin <tux255@gmail.com>
2022-07-13 13:32:21 +0300

    Button tests

---
+++ src/components/ui/Buttons/__tests__/Button.test.jsx
@@ -0,0 +1,23 @@
+import Button from "../Button";
+import { render, screen } from "@testing-library/react";
+import useEvent from "@testing-library/user-event";
+
+describe("Button", () => {
+  const clickHandler = jest.fn();
+  beforeEach(() => {
+    clickHandler.mockClear();
+  });
+  it("should be in the document", () => {
+    render(<Button title="test button" onClick={clickHandler} />);
+
+    expect(screen.getByRole("button")).toMatchSnapshot();
+  });
+
+  it("should handle click", async () => {
+    render(<Button title="test button" onClick={clickHandler} />);
+
+    await useEvent.click(screen.getByRole("button"));
+
+    expect(clickHandler).toHaveBeenCalledTimes(1);
+  });
+});
