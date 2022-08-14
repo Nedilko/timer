9aff201b4760f24868350ce6dac39bfef219cf97
Andrii Haranin <tux255@gmail.com>
2022-08-14 18:16:31 +0300

    OverlayBackground test

--- src/components/ui/Modals/OverlayBackgrounds/OverlayBackground.jsx
+++ src/components/ui/Modals/OverlayBackgrounds/OverlayBackground.jsx
@@ -1,15 +1,16 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 OverlayBackground.propTypes = {
-  children: PropTypes.object,
+  children: PropTypes.node,
   onClick: PropTypes.func,
 };
 
 function OverlayBackground({ children, onClick }) {
   return (
     <div
+      role="overlay"
       onClick={onClick}
-      className="fixed inset-0	 backdrop-blur-sm w-full h-full flex items-center justify-center"
+      className="fixed inset-0 backdrop-blur-sm w-full h-full flex items-center justify-center"
     >
       {children}
     </div>
